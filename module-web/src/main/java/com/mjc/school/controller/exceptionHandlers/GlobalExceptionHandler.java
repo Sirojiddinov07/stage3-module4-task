7c65c2012b7c7645eac47c203d053c59e79a6484
Firstname Lastname <ssirojiddinov777@gmail.com>
2023-08-31 11:59:20 +0500

    some changes added

--- module-web/src/main/java/com/mjc/school/controller/exceptionHandlers/GlobalExceptionHandler.java
+++ module-web/src/main/java/com/mjc/school/controller/exceptionHandlers/GlobalExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
     protected ResponseEntity<Object> handleBadRequest(RuntimeException ex, WebRequest request){
